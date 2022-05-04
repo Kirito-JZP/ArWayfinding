bc5df2e5ffb2db665d073a30d8bb28c31b2c88ac
JIA <jiazh@tcd.ie>
2022-05-04 03:49:33 +0100

    Add dto copy library and static strings

---
+++ app/src/main/java/com/main/arwayfinding/utility/StaticStringUtils.java
@@ -0,0 +1,32 @@
+package com.main.arwayfinding.utility;
+
+
+public class StaticStringUtils {
+    public static final String NULL_STRING = "";
+    public static final String PRIVACY_TITLE = "Privacy Protocol";
+    public static final String PRIVACY_TXT = "privacy.txt";
+    // Dialog Message
+    public static final String TERMS_AGREE_MSG = "Please agree with terms";
+    public static final String INCORRECT_EMAIL_FORMAT = "Incorrect Email Format!\n";
+    public static final String EMPTY_STRING = " is empty\n";
+    public static final String INCORRECT_PASSWORD_FORMAT = "Incorrect Password Format!\n";
+    public static final String AUTHORITY_FAIL_STRING = "The password is invalid or the user does " +
+            "not have a password.";
+    public static final String AUTHORITY_FAIL_MSG = "Incorrect username or password!";
+    public static final String ADD_SUCCESS_MSG = "Added a destination!";
+    public static final String NO_INPUT_MSG = "No inputs in destination!";
+    public static final String GET_LATEST_LOCATION_FAIL = "Unable to get the latest location";
+    public static final String START_NAVIGATION = "Navigation started";
+    public static final String RESUME_NAVIGATION = "Navigation resumed";
+    public static final String PAUSE_NAVIGATION = "Navigation paused";
+    public static final String STOP_NAVIGATION = "Navigation stopped";
+    public static final String ARRIVED = "You have reached your destination";
+    public static final String NO_AVAILABLE_ROUTE = "No routes found for selected transporation " +
+            "means";
+    public static final String NO_ACCOUNT_FOUND = "We couldn't find an account with that email " +
+            "address.";
+    public static final String EMAIL_SENT = "Email sent!";
+    public static final String SIGNUP_ERROR = "This email have already registered.";
+    public static final String ERROR = "Error";
+
+}
